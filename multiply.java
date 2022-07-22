7dea80e8e51aeb6b41d1d098263b351904e71446
devx404 <devilbhoir03@gmail.com>
2022-07-22 12:20:16 +0530

    added multi.java

---
+++ multiply.java
@@ -0,0 +1,11 @@
+public class  multiply{
+
+
+    public static void main(String[ ] args){
+
+        string v = "vijay";
+        string s ="snehal";
+
+        System.out.println(v + "Love A lot " + s)
+    }
+}
